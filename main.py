e430d364a3280e909633e65c56984d074bc4f7c0
Dishang Mehta <78553905+DishangMehta11@users.noreply.github.com>
2021-07-05 10:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import hashlib
 
-string = "harsh"
-result = hashlib.md5(string.encode())
 
-print("The hexadecimal equivalent of hash is: ", result.hexdigest())
+def str2md5(string_p):
+    result = hashlib.md5(string_p.encode())
+    return result.hexdigest()
+
+
+string_v = input('Enter the string you want to encrypt: ')
+print(f'The MD5 encyption of {string_v} is {str2md5(string_v)}')
